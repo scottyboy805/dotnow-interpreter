f7e80c3027aaf95f7c4157b8683efd4e72d6cb7f
scottyboy805 <scottyboy805@hotmail.co.uk>
2021-08-25 14:45:14 +0100

    Remove old code and unused using statements

--- Assets/dotnow/Tools/BindingGenerator/Scripts/BindingsGeneratorService.cs
+++ Assets/dotnow/Tools/BindingGenerator/Scripts/BindingsGeneratorService.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.CodeDom;
-using System.CodeDom.Compiler;
-using System.IO;
 using System.Reflection;
-using System.Text;
 using dotnow.BindingGenerator.Emit;
 
 namespace dotnow.BindingGenerator
@@ -49,75 +45,6 @@ namespace dotnow.BindingGenerator
                 ProxyGenerator.GenerateProxyDefinitionsForType(type, outputFolderOrPath, result);
 
             return result;
-        }
-
-        //public BindingsGeneratorResult GenerateProxyDefinitionsForAssembly(Assembly assembly, string generateInFolder)
-        //{
-            
-
-        //    return result;
-        //}
-
-        //public BindingsGeneratorResult GenerateProxyDefinitionsForType(Type type, string generateInFolder)
-        //{
-        //    // Validate
-        //    if (type == null) throw new ArgumentNullException(nameof(type));
-        //    if (string.IsNullOrEmpty(generateInFolder) == true) throw new ArgumentException(nameof(generateInFolder) + " cannot be null or empty");
-
-        //    // Create result
-        //    BindingsGeneratorResult result = new BindingsGeneratorResult(generateInFolder);
-
-        //    // Call through
-        //    if (CanGenerateProxyForType(type) == true)
-        //    {
-        //        GenerateProxyDefinitionsForTypeImpl(type, result);
-        //    }
-
-        //    // Check for no types
-        //    if (result.GeneratedSourceFileCount == 0)
-        //        result.SetError("No suitable types found! Proxies will only be generated for interface, abstract or non-sealed types which define one or more virtual or abstract members");
-
-        //    return result;
-        //}
-
-        //private void GenerateProxyDefinitionsForTypeImpl(Type type, BindingsGeneratorResult result)
-        //{
-        //    ProxyTypeBuilder typeBuilder = new ProxyTypeBuilder(type);
-
-        //    UnityEngine.Debug.Log(type.Assembly.Location);
-
-        //    // Get the output name
-        //    string outputName = string.Concat(typeBuilder.GetTypeFlattenedName(), ".cs");
-
-        //    CodeDomProvider provider = CodeDomProvider.CreateProvider("C#");
-
-        //    StringBuilder builder = new StringBuilder();
-
-        //    using (TextWriter writer = new StringWriter(builder))
-        //    {
-        //        CodeCompileUnit compileUnit = new CodeCompileUnit();
-        //        compileUnit.Namespaces.Add(new CodeNamespace("generated"));
-
-        //        compileUnit.Namespaces[0].Types.Add(typeBuilder.BuildTypeProxy());
-
-        //        provider.GenerateCodeFromCompileUnit(compileUnit, writer, options);
-        //    }
-
-        //    // Get output source string
-        //    string output = builder.ToString();
-
-        //    // Get output path
-        //    string outputPath = Path.Combine(result.GenerateInFolder, outputName);
-
-        //    DirectoryInfo parent = Directory.GetParent(outputPath);
-
-        //    if (parent.Exists == false)
-        //        parent.Create();
-
-        //    // Write to file
-        //    File.WriteAllText(outputPath, output);
-        //}
-
-        
+        }        
     }
 }
