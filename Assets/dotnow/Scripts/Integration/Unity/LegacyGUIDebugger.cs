0e053e90cea05183ec5749c8494cf247c60c4472
scottyboy805 <scottyboy805@hotmail.co.uk>
2021-10-15 13:35:03 +0100

    Refactor legacy guid debugger for better performance and auto-attachment

--- Assets/dotnow/Scripts/Integration/Unity/LegacyGUIDebugger.cs
+++ Assets/dotnow/Scripts/Integration/Unity/LegacyGUIDebugger.cs
@@ -11,7 +11,8 @@ namespace dotnow
         private AppDomain domain = null;
         private ExecutionEngine engine = null;
         private DebugFrame debugFrame = null;
-        private Vector2 scroll = Vector2.zero;
+        private Vector2 scrollInstructions = Vector2.zero;
+        private Vector3 scrollEvalStack = Vector2.zero;
 
         // Public
         public bool pauseExecutionOnStart = false;
@@ -38,10 +39,22 @@ namespace dotnow
         }
 
 #if UNITY_EDITOR
+        public void Start()
+        {
+            // Add listener
+            AppDomain.OnDomainCreated += (AppDomain domain) =>
+            {
+                domain.AttachDebugger(this);
+            };
+        }
+
         public void OnGUI()
         {
             if (engine == null || engine.IsDebuggerPaused == false)
                 return;
+
+            // Set gui scale
+            GUI.matrix = Matrix4x4.Scale(Vector3.one * 2);
 
             GUIStyle labelStyle = new GUIStyle(EditorStyles.miniLabel);
             labelStyle.normal.textColor = Color.white;
@@ -51,6 +64,20 @@ namespace dotnow
             {
                 GUILayout.BeginVertical();
                 {
+                    GUILayout.BeginHorizontal();
+                    {
+                        if (GUILayout.Button("Step Once (F10)") == true || Input.GetKeyDown(KeyCode.F10) == true)
+                        {
+                            engine.StepExecution();
+                        }
+
+                        if (GUILayout.Button("Continue (F5)") == true || (Event.current.type == EventType.KeyDown && Input.GetKeyDown(KeyCode.F5) == true))
+                        {
+                            engine.ContinueExecution();
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+
                     // Main instruction set layout
                     GUILayout.BeginVertical(GUI.skin.box);
                     {
@@ -62,11 +89,11 @@ namespace dotnow
                     Color old = GUI.contentColor;
 
                     // Start scroll view
-                    scroll = GUILayout.BeginScrollView(scroll, GUI.skin.box);
+                    scrollInstructions = GUILayout.BeginScrollView(scrollInstructions, GUI.skin.box);
                     {
                         for (int i = 0; i < debugFrame.InstructionSet.Length; i++)
                         {
-                            if (i == debugFrame.ExecutingFrame.instructionPtr)
+                            if (i == debugFrame.ExecutingInstructionIndex)
                             {
                                 GUI.contentColor = Color.red;
                                 GUILayout.Box("[" + i + "]: " + debugFrame.InstructionSet[i].ToString(), labelStyle);
@@ -81,19 +108,7 @@ namespace dotnow
                     GUILayout.EndScrollView();
 
 
-                    GUILayout.BeginHorizontal();
-                    {
-                        if (GUILayout.Button("Step Once (F10)") == true || Input.GetKeyDown(KeyCode.F10) == true)
-                        {
-                            engine.StepExecution();
-                        }
-
-                        if(GUILayout.Button("Continue (F5)") == true || (Event.current.type == EventType.KeyDown && Input.GetKeyDown(KeyCode.F5) == true))
-                        {
-                            engine.ContinueExecution();
-                        }
-                    }
-                    GUILayout.EndHorizontal();
+                    
                 }
                 GUILayout.EndVertical();
                 // Instruction set layout
@@ -103,31 +118,39 @@ namespace dotnow
                 // Main stack layout
                 GUILayout.BeginVertical(GUI.skin.box);
                 {
-                    GUILayout.Label("Evaluation Stack", EditorStyles.whiteLargeLabel);
-
-                    GUILayout.Space(10);
-                    GUILayout.Label("Index = " + debugFrame.ExecutingFrame.stackIndex);
-                    GUILayout.Label("Max Size = " + debugFrame.ExecutingFrame.stack.Length);
-
-
-                    // Stack elements
-                    GUILayout.Space(10);
-                    GUILayout.Label("Stack data");
-
-                    Color old = GUI.contentColor;
-                    for(int i = 0; i < debugFrame.ExecutingFrame.stack.Length; i++)
+                    scrollEvalStack = GUILayout.BeginScrollView(scrollEvalStack);
                     {
-                        if(i == debugFrame.ExecutingFrame.stackIndex - 1)
+                        GUILayout.Label("Evaluation Stack", EditorStyles.whiteLargeLabel);
+
+                        GUILayout.Space(10);
+                        GUILayout.Label("Index = " + debugFrame.ExecutingFrame.stackIndex);
+                        GUILayout.Label("Max Size = " + debugFrame.ExecutingFrame.stack.Length);
+
+
+                        // Stack elements
+                        GUILayout.Space(10);
+                        GUILayout.Label("Stack data");
+
+                        Color old = GUI.contentColor;
+                        for (int i = 0; i < debugFrame.ExecutingFrame.stack.Length; i++)
                         {
-                            GUI.contentColor = Color.red;
-                            GUILayout.Box("[" + i + "]: " + debugFrame.ExecutingFrame.stack[i], labelStyle);
+                            // Check for stack max
+                            if (i > debugFrame.ExecutingFrame.stackMax)
+                                break;
+
+                            if (i == debugFrame.ExecutingFrame.stackIndex - 1)
+                            {
+                                GUI.contentColor = Color.red;
+                                GUILayout.Box("[" + i + "]: " + debugFrame.ExecutingFrame.stack[i], labelStyle);
+                            }
+                            else
+                            {
+                                GUILayout.Label("[" + i + "]: " + debugFrame.ExecutingFrame.stack[i], labelStyle);
+                            }
+                            GUI.contentColor = old;
                         }
-                        else
-                        {
-                            GUILayout.Label("[" + i + "]: " + debugFrame.ExecutingFrame.stack[i], labelStyle);
-                        }
-                        GUI.contentColor = old;
                     }
+                    GUILayout.EndScrollView();
                 }
                 GUILayout.EndVertical();
 
