d8eb512f7e9e30d91cdfcaf90e3caec4137fc1eb
scottyboy805 <scottyboy805@hotmail.co.uk>
2022-01-05 14:58:04 +0000

    Add extra compile methods (non-async) to mimic Roslyn C# API.

--- Assets/dotnow/Scripts/Integration/RoslynCSharp/ScriptDomainExtensions.cs
+++ Assets/dotnow/Scripts/Integration/RoslynCSharp/ScriptDomainExtensions.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using dotnow.Reflection;
 using AppDomain = dotnow.AppDomain;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace RoslynCSharp
 {
@@ -58,7 +59,31 @@ namespace RoslynCSharp
         public static ScriptType CompileAndLoadMainSourceInterpreted(this ScriptDomain domain, string cSharpSource, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
         {
             // Send compile request
-            ScriptAssembly assembly = CompileAndLoadSourceInterpreted(domain, cSharpSource, securityMode, additionalReferenceAssemblies);
+            ScriptAssembly assembly = CompileAndLoadSourcesInterpreted(domain, new string[] { cSharpSource }, securityMode, additionalReferenceAssemblies);
+
+            // Try to get main type
+            if (assembly != null && assembly.MainType != null)
+                return assembly.MainType;
+
+            return null;
+        }
+
+        public static ScriptType CompileAndLoadMainFileInterpreted(this ScriptDomain domain, string cSharpFile, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
+        {
+            // Send compile request
+            ScriptAssembly assembly = CompileAndLoadFilesInterpreted(domain, new string[] { cSharpFile }, securityMode, additionalReferenceAssemblies);
+
+            // Try to get main type
+            if (assembly != null && assembly.MainType != null)
+                return assembly.MainType;
+
+            return null;
+        }
+
+        public static ScriptType CompileAndLoadMainSyntaxTreeInterpreted(this ScriptDomain domain, CSharpSyntaxTree cSharpSyntaxTree, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
+        {
+            // Send compile request
+            ScriptAssembly assembly = CompileAndLoadSyntaxTreesInterpreted(domain, new CSharpSyntaxTree[] { cSharpSyntaxTree }, securityMode, additionalReferenceAssemblies);
 
             // Try to get main type
             if (assembly != null && assembly.MainType != null)
@@ -69,6 +94,11 @@ namespace RoslynCSharp
 
         public static ScriptAssembly CompileAndLoadSourceInterpreted(this ScriptDomain domain, string cSharpSource, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
         {
+            return CompileAndLoadSourcesInterpreted(domain, new string[] { cSharpSource }, securityMode, additionalReferenceAssemblies);
+        }
+
+        public static ScriptAssembly CompileAndLoadSourcesInterpreted(this ScriptDomain domain, string[] cSharpSources, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
+        {
             // Check for disposed
             if(domain.IsDisposed == true)
                 throw new ObjectDisposedException(nameof(domain));
@@ -76,14 +106,14 @@ namespace RoslynCSharp
             // Check for compiler
             if (domain.IsCompilerServiceInitialized == false)
                 throw new InvalidOperationException("The compiler service has not been initialized");
-            UnityEngine.Debug.Log("Before compile");
+
             // Send the compile request
             bool loadAssemblies = RoslynCSharpCompiler.loadCompiledAssemblies;
 
             RoslynCSharpCompiler.loadCompiledAssemblies = false;
-            domain.CompileFromSource(cSharpSource, additionalReferenceAssemblies);
+            domain.CompileFromSources(cSharpSources, additionalReferenceAssemblies);
             RoslynCSharpCompiler.loadCompiledAssemblies = loadAssemblies;
-            UnityEngine.Debug.Log("After compile");
+
             // Log output to console
             domain.LogCompilerOutputToConsole();
 
@@ -104,7 +134,93 @@ namespace RoslynCSharp
             // Register assembly
             return domain.RegisterAssembly<InterpretedScriptCompiledAssembly>(module, securityMode, domain.CompileResult);
         }
-#endregion
+
+        public static ScriptAssembly CompileAndLoadFileInterpreted(this ScriptDomain domain, string cSharpFile, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
+        {
+            return CompileAndLoadFilesInterpreted(domain, new string[] { cSharpFile }, securityMode, additionalReferenceAssemblies);
+        }
+
+        public static ScriptAssembly CompileAndLoadFilesInterpreted(this ScriptDomain domain, string[] cSharpFiles, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
+        {
+            // Check for disposed
+            if (domain.IsDisposed == true)
+                throw new ObjectDisposedException(nameof(domain));
+
+            // Check for compiler
+            if (domain.IsCompilerServiceInitialized == false)
+                throw new InvalidOperationException("The compiler service has not been initialized");
+
+            // Send the compile request
+            bool loadAssemblies = RoslynCSharpCompiler.loadCompiledAssemblies;
+
+            RoslynCSharpCompiler.loadCompiledAssemblies = false;
+            domain.CompileFromFiles(cSharpFiles, additionalReferenceAssemblies);
+            RoslynCSharpCompiler.loadCompiledAssemblies = loadAssemblies;
+
+            // Log output to console
+            domain.LogCompilerOutputToConsole();
+
+            // Get app domain
+            AppDomain appDomain = GetAppDomain(domain);
+
+            CLRModule module = null;
+
+            if (domain.CompileResult.Success == true)
+            {
+                // Create stream
+                MemoryStream stream = new MemoryStream(domain.CompileResult.OutputAssemblyImage);
+
+                // Load the assembly into domain
+                module = appDomain.LoadModuleStream(stream, false);
+            }
+
+            // Register assembly
+            return domain.RegisterAssembly<InterpretedScriptCompiledAssembly>(module, securityMode, domain.CompileResult);
+        }
+
+        public static ScriptAssembly CompileAndLoadSyntaxTreeInterpreted(this ScriptDomain domain, CSharpSyntaxTree syntaxTree, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
+        {
+            return CompileAndLoadSyntaxTreesInterpreted(domain, new CSharpSyntaxTree[] { syntaxTree }, securityMode, additionalReferenceAssemblies);
+        }
+
+        public static ScriptAssembly CompileAndLoadSyntaxTreesInterpreted(this ScriptDomain domain, CSharpSyntaxTree[] cSharpSyntaxTrees, ScriptSecurityMode securityMode = ScriptSecurityMode.UseSettings, IMetadataReferenceProvider[] additionalReferenceAssemblies = null)
+        {
+            // Check for disposed
+            if (domain.IsDisposed == true)
+                throw new ObjectDisposedException(nameof(domain));
+
+            // Check for compiler
+            if (domain.IsCompilerServiceInitialized == false)
+                throw new InvalidOperationException("The compiler service has not been initialized");
+
+            // Send the compile request
+            bool loadAssemblies = RoslynCSharpCompiler.loadCompiledAssemblies;
+
+            RoslynCSharpCompiler.loadCompiledAssemblies = false;
+            domain.CompileFromSyntaxTrees(cSharpSyntaxTrees, additionalReferenceAssemblies);
+            RoslynCSharpCompiler.loadCompiledAssemblies = loadAssemblies;
+
+            // Log output to console
+            domain.LogCompilerOutputToConsole();
+
+            // Get app domain
+            AppDomain appDomain = GetAppDomain(domain);
+
+            CLRModule module = null;
+
+            if (domain.CompileResult.Success == true)
+            {
+                // Create stream
+                MemoryStream stream = new MemoryStream(domain.CompileResult.OutputAssemblyImage);
+
+                // Load the assembly into domain
+                module = appDomain.LoadModuleStream(stream, false);
+            }
+
+            // Register assembly
+            return domain.RegisterAssembly<InterpretedScriptCompiledAssembly>(module, securityMode, domain.CompileResult);
+        }
+        #endregion
 
         public static AppDomain GetAppDomain(this ScriptDomain domain)
         {
