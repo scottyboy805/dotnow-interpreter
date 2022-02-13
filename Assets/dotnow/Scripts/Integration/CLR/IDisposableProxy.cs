1bd2fd2b7fb5745ae8410681cefbb7d1979c2531
scottyboy805 <scottyboy805@hotmail.co.uk>
2022-02-13 13:53:40 +0000

    Add proxy bindings for interfaces used by compiler generated state machines

---
+++ Assets/dotnow/Scripts/Integration/CLR/IDisposableProxy.cs
@@ -0,0 +1,22 @@
+﻿using dotnow;
+using dotnow.Interop;
+
+namespace System
+{
+    [CLRProxyBinding(typeof(IDisposable))]
+    public class IDisposableProxy : ICLRProxy, IDisposable
+    {
+        // Private
+        private CLRInstance instance;
+
+        public void Dispose()
+        {
+            instance.Type.GetMethod("Dispose")?.Invoke(instance, null);
+        }
+
+        public void InitializeProxy(dotnow.AppDomain domain, CLRInstance instance)
+        {
+            this.instance = instance;
+        }
+    }
+}
---
+++ Assets/dotnow/Scripts/Integration/CLR/IEnumerator(Object)Proxy.cs
@@ -0,0 +1,40 @@
+﻿using dotnow;
+using dotnow.Interop;
+
+namespace System.Collections.Generic
+{
+    [CLRProxyBinding(typeof(IEnumerator<object>))]
+    public class IEnumerator_Object_Proxy : ICLRProxy, IEnumerator<object>
+    {
+        // Private
+        private CLRInstance instance;
+
+        public object Current
+        {
+            get
+            {
+                return instance.Type.GetMethod("System.Collections.Generic.IEnumerator<System.Object>.get_Current")?.Invoke(instance, null);
+            }
+        }
+
+        public void Dispose()
+        {
+            instance.Type.GetMethod("Dispose")?.Invoke(instance, null);
+        }
+
+        public void InitializeProxy(dotnow.AppDomain domain, CLRInstance instance)
+        {
+            this.instance = instance;
+        }
+
+        public bool MoveNext()
+        {
+            return (bool)instance.Type.GetMethod("MoveNext")?.Invoke(instance, null);
+        }
+
+        public void Reset()
+        {
+            instance.Type.GetMethod("Reset")?.Invoke(instance, null);
+        }
+    }
+}
