1ba49c1462ae34326a73d5cb9f65660773da11f8
scottyboy805 <scottyboy805@hotmail.co.uk>
2022-04-03 19:17:14 +0100

    Start implementing custom inspector for interpreted scripts

--- Assets/dotnow/Scripts/Editor/MonoBehaviourProxyEditor.cs
+++ Assets/dotnow/Scripts/Editor/MonoBehaviourProxyEditor.cs
@@ -1,26 +1,50 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEditor;
-//using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
-//[CustomEditor(typeof(MonoBehaviourProxy))]
-//public class MonoBehaviourProxyEditor : Editor
-//{
-//    public override string GetInfoString()
-//    {
-//        return "Hello World";
-//    }
+namespace dotnow
+{
+    [CustomEditor(typeof(MonoBehaviourProxy), true)]
+    public class MonoBehaviourProxyEditor : Editor
+    {
+        // Private
+        private MonoBehaviourProxy proxy = null;
+        private CLRInstance instance = null;
+        private CLRType type = null;
 
-//    public override void OnInspectorGUI()
-//    {
-//        DrawHeader();
-//        GUILayout.Label("Test");
-//        DrawHeader();
-//        //base.OnInspectorGUI();
-//    }
+        private MonoScript fakeScriptInfo = null;
 
-//    public override GUIContent GetPreviewTitle()
-//    {
-//        return new GUIContent("Test");
-//    }
-//}
+        // Methods
+        private void OnEnable()
+        {
+            // Get the proxy class
+            proxy = target as MonoBehaviourProxy;
+
+            // Get the instance and type
+            instance = proxy.Instance;
+            type = instance.Type;
+
+            // Create dummy script info
+            fakeScriptInfo = new MonoScript();
+            fakeScriptInfo.name = type.Name + " (Interpreted)";
+        }
+
+        public override void OnInspectorGUI()
+        {
+            // Draw script field
+            DrawScriptField();
+        }
+
+        private void DrawScriptField()
+        {
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.BeginHorizontal();
+            {
+                // Script field
+                GUILayout.Label("Script", GUILayout.Width(EditorGUIUtility.labelWidth));
+                EditorGUILayout.ObjectField(fakeScriptInfo, typeof(MonoScript));
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}
