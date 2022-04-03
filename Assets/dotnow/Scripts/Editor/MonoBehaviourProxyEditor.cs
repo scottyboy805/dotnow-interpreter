bb95098491f60090c8226e0f51dadcd0888ed33c
scottyboy805 <scottyboy805@hotmail.co.uk>
2022-04-03 19:47:24 +0100

    Add custom drawers for primitive types in monobehaviour proxy editor

--- Assets/dotnow/Scripts/Editor/MonoBehaviourProxyEditor.cs
+++ Assets/dotnow/Scripts/Editor/MonoBehaviourProxyEditor.cs
@@ -1,4 +1,7 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 namespace dotnow
@@ -6,7 +9,17 @@ namespace dotnow
     [CustomEditor(typeof(MonoBehaviourProxy), true)]
     public class MonoBehaviourProxyEditor : Editor
     {
+        // Delegate
+        private delegate void CustomDrawer(FieldInfo field, object instance);
+
         // Private
+        private static Dictionary<Type, CustomDrawer> fieldDrawers = new Dictionary<Type, CustomDrawer>
+        {
+            { typeof(string), DrawStringField },
+            { typeof(int), DrawIntField },
+            { typeof(float), DrawFloatField },
+        };
+
         private MonoBehaviourProxy proxy = null;
         private CLRInstance instance = null;
         private CLRType type = null;
@@ -32,6 +45,9 @@ namespace dotnow
         {
             // Draw script field
             DrawScriptField();
+
+            // Draw all serialize fields
+            DrawSerializeFields();
         }
 
         private void DrawScriptField()
@@ -45,6 +61,92 @@ namespace dotnow
             }
             EditorGUILayout.EndHorizontal();
             EditorGUI.EndDisabledGroup();
+        }
+
+        private void DrawSerializeFields()
+        {
+            // Get all serializable
+            foreach(FieldInfo field in type.GetInstanceFields())
+            {
+                // Check for non-public without attribute
+                if (field.IsPublic == false && field.IsDefined(typeof(SerializeField), false) == false)
+                    continue;
+
+                // Check for explicitly ignored
+                if (field.IsDefined(typeof(NonSerializedAttribute), false) == true)
+                    continue;
+
+
+                // Draw field
+                GUILayout.BeginHorizontal();
+                {
+                    // Draw label
+                    GUILayout.Label(field.Name, GUILayout.Width(EditorGUIUtility.labelWidth));
+
+
+                    // Draw correct field
+                    CustomDrawer drawer = null;
+                    if(fieldDrawers.TryGetValue(field.FieldType, out drawer) == true)
+                    {
+                        // Update the drawer
+                        drawer(field, instance);
+                    }
+                    else
+                    {
+                        EditorGUILayout.HelpBox("Unable to display serialized field!", MessageType.Warning);
+                    }
+
+                }
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        private static void DrawStringField(FieldInfo field, object instance)
+        {
+            // Get the string value from instance
+            string text = (string)field.GetValue(instance);
+
+            // Display gui
+            string inputText = EditorGUILayout.TextField(text);
+
+            // Check for changed
+            if(text != inputText)
+            {
+                // Update instance field
+                field.SetValue(instance, inputText);
+            }
+        }
+
+        private static void DrawIntField(FieldInfo field, object instance)
+        {
+            // Get the int value from instance
+            int val = (int)field.GetValue(instance);
+
+            // Display gui
+            int inputVal = EditorGUILayout.IntField(val);
+
+            // Check for changed
+            if(val != inputVal)
+            {
+                // Update instance field
+                field.SetValue(instance, inputVal);
+            }
+        }
+
+        private static void DrawFloatField(FieldInfo field, object instance)
+        {
+            // Get the float value from instance
+            float val = (float)field.GetValue(instance);
+
+            // Display gui
+            float inputVal = EditorGUILayout.FloatField(val);
+
+            // Check for changed
+            if (val != inputVal)
+            {
+                // Update instance field
+                field.SetValue(instance, inputVal);
+            }
         }
     }
 }
