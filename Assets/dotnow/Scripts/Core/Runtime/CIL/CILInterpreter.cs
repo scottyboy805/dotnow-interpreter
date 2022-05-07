4c96dba510796539b989372c37ce9fcb021ec5e1
scottyboy805 <scottyboy805@hotmail.co.uk>
2022-05-07 15:35:06 +0100

    Promote small primitives to 32-bit during add instruction

--- Assets/dotnow/Scripts/Core/Runtime/CIL/CILInterpreter.cs
+++ Assets/dotnow/Scripts/Core/Runtime/CIL/CILInterpreter.cs
@@ -81,6 +81,7 @@ namespace dotnow.Runtime.CIL
                                 case StackData.ObjectType.Int16:
                                 case StackData.ObjectType.Int32:
                                     stack[stackPtr - 2].value.Int32 = unchecked(left.value.Int32 + right.value.Int32);
+                                    stack[stackPtr - 2].type = StackData.ObjectType.Int32;
                                     break;
 
                                 case StackData.ObjectType.UInt8:
