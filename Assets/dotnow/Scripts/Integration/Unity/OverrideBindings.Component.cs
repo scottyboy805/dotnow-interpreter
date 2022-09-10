fd45ea795e499b9fcf771892bb609a59d495d8c6
DESKTOP-591D8U6\scott <scottyboy805@hotmail.co.uk>
2022-09-10 16:09:35 +0100

    Add get components override

--- Assets/dotnow/Scripts/Integration/Unity/OverrideBindings.Component.cs
+++ Assets/dotnow/Scripts/Integration/Unity/OverrideBindings.Component.cs
@@ -43,6 +43,42 @@ namespace UnityEngine
             // No component found
             return null;
         }
+
+        [Preserve]
+        [CLRMethodBinding(typeof(Component), "GetComponents", typeof(Type))]
+        public static object UnityEngine_Component_GetComponentsOverride(dotnow.AppDomain domain, MethodInfo overrideMethod, object instance, object[] args)
+        {
+            // Get component
+            Component comp = instance as Component;
+
+            // Get argument
+            Type componentType = args[0] as Type;
+
+            // Check for clr type
+            if (componentType.IsCLRType() == false)
+            {
+                return comp.GetComponents(componentType);
+            }
+
+            // Get all proxies
+            getComponentsNonAlloc.Clear();
+            comp.GetComponents<ICLRProxy>(getComponentsNonAlloc);
+
+            // Get results collection
+            List<Component> results = new List<Component>(getComponentsNonAlloc.Count);
+
+            // Check for matching instance
+            for (int i = 0; i < getComponentsNonAlloc.Count; i++)
+            {
+                // Check for matching type then return the clr instance
+                if (getComponentsNonAlloc[i].Instance.Type == componentType)
+                    results.Add(getComponentsNonAlloc[i].Instance.Unwrap() as Component);
+            }
+
+            // No component found
+            return results.ToArray();
+
+        }
     }
 }
 #endif
