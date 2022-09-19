08e7444462b2b74263bd52d3544fa5109e07547b
DESKTOP-591D8U6\scott <scottyboy805@hotmail.co.uk>
2022-09-19 08:40:13 +0100

    Add get component overrides for game object

--- Assets/dotnow/Scripts/Integration/Unity/OverrideBindings.GameObject.cs
+++ Assets/dotnow/Scripts/Integration/Unity/OverrideBindings.GameObject.cs
@@ -1,6 +1,7 @@
 ﻿#if !UNITY_DISABLE
 #if (UNITY_EDITOR || UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID || UNITY_WSA || UNITY_WEBGL)
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using dotnow;
 using dotnow.Interop;
@@ -38,6 +39,74 @@ namespace UnityEngine
 
             // Create clr instance
             return domain.CreateInstanceFromProxy(componentType, proxyInstance);
+        }
+
+
+        [Preserve]
+        [CLRMethodBinding(typeof(GameObject), "GetComponent", typeof(Type))]
+        public static object UnityEngine_GameObject_GetComponentOverride(dotnow.AppDomain domain, MethodInfo overrideMethod, object instance, object[] args)
+        {
+            // Get object
+            GameObject go = instance as GameObject;
+
+            // Get argument
+            Type componentType = args[0] as Type;
+
+            // Check for clr type
+            if (componentType.IsCLRType() == false)
+            {
+                return go.GetComponent(componentType);
+            }
+
+            // Get all proxies
+            getComponentsNonAlloc.Clear();
+            go.GetComponents<ICLRProxy>(getComponentsNonAlloc);
+
+            // Check for matching instance
+            for (int i = 0; i < getComponentsNonAlloc.Count; i++)
+            {
+                // Check for matching type then return the clr instance
+                if (getComponentsNonAlloc[i].Instance.Type == componentType)
+                    return getComponentsNonAlloc[i].Instance;
+            }
+
+            // No component found
+            return null;
+        }
+
+        [Preserve]
+        [CLRMethodBinding(typeof(GameObject), "GetComponents", typeof(Type))]
+        public static object UnityEngine_GameObject_GetComponentsOverride(dotnow.AppDomain domain, MethodInfo overrideMethod, object instance, object[] args)
+        {
+            // Get object
+            GameObject go = instance as GameObject;
+
+            // Get argument
+            Type componentType = args[0] as Type;
+
+            // Check for clr type
+            if (componentType.IsCLRType() == false)
+            {
+                return go.GetComponents(componentType);
+            }
+
+            // Get all proxies
+            getComponentsNonAlloc.Clear();
+            go.GetComponents<ICLRProxy>(getComponentsNonAlloc);
+
+            // Get results collection
+            List<Component> results = new List<Component>(getComponentsNonAlloc.Count);
+
+            // Check for matching instance
+            for (int i = 0; i < getComponentsNonAlloc.Count; i++)
+            {
+                // Check for matching type then return the clr instance
+                if (getComponentsNonAlloc[i].Instance.Type == componentType)
+                    results.Add(getComponentsNonAlloc[i].Instance.Unwrap() as Component);
+            }
+
+            // No component found
+            return results.ToArray();
         }
     }
 }
