178411eeb8412676793f865649362ab750dacc62
DESKTOP-591D8U6\scott <scottyboy805@hotmail.co.uk>
2023-03-10 09:59:23 +0000

    Fix usings

--- Assets/dotnow/Scripts/Core/Runtime/__delegate.cs
+++ Assets/dotnow/Scripts/Core/Runtime/__delegate.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using UnityEngine.Windows.Speech;
 
 namespace dotnow.Runtime
 {
