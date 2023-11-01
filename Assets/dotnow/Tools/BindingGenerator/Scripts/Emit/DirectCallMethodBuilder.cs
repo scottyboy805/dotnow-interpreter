e0478ac3a27a6796220bb86f077b3198271c13d4
DESKTOP-591D8U6\scott <scottyboy805@hotmail.co.uk>
2023-11-01 10:19:26 +0000

    Start adding support for generating direct call bindings

---
+++ Assets/dotnow/Tools/BindingGenerator/Scripts/Emit/DirectCallMethodBuilder.cs
@@ -0,0 +1,57 @@
+﻿#if !UNITY_DISABLE
+#if UNITY_EDITOR && NET_4_6
+using dotnow.Runtime;
+using System;
+using System.CodeDom;
+using System.Reflection;
+
+namespace dotnow.BindingGenerator.Emit
+{
+    internal sealed class DirectCallMethodBuilder
+    {
+        // Private
+        private MethodInfo method = null;
+
+        // Public
+        public const string stackArg = "stack";
+        public const string offsetArg = "offset";
+
+        // Constructor
+        public DirectCallMethodBuilder(MethodInfo method)
+        {
+            this.method = method;
+        }
+
+        // Methods
+        public CodeMemberMethod BuildMethodDirectCall()
+        {
+            CodeMemberMethod codeMethod = new CodeMemberMethod();
+            codeMethod.Name = (method.DeclaringType.Namespace != null)
+                ? method.DeclaringType.Namespace.Replace('.', '_') + "_" + method.DeclaringType.Name + "_" + method.Name
+                : method.DeclaringType.Name + "_" + method.Name;
+            codeMethod.Attributes = MemberAttributes.Assembly | MemberAttributes.Static | MemberAttributes.Final;
+
+            // Prevent Unity code stripping
+            codeMethod.CustomAttributes.Add(new CodeAttributeDeclaration(new CodeTypeReference(typeof(PreserveAttribute))));
+            
+            // Register binding with dotnow
+            codeMethod.CustomAttributes.Add(new CodeAttributeDeclaration(new CodeTypeReference(typeof(CLRMethodDirectCallBindingAttribute)),
+                new CodeAttributeArgument(new CodeTypeOfExpression(method.DeclaringType)), new CodeAttributeArgument(new CodeSnippetExpression("\"" + method.Name + "\""))));
+
+            // Stack parameter
+            codeMethod.Parameters.Add(new CodeParameterDeclarationExpression(
+                new CodeTypeReference(typeof(StackData[])), stackArg));
+
+            // Offset parameter
+            codeMethod.Parameters.Add(new CodeParameterDeclarationExpression(
+                new CodeTypeReference(typeof(int)), offsetArg));
+
+
+            // TODO - method body impl
+
+            return codeMethod;
+        }
+    }
+}
+#endif
+#endif
---
+++ Assets/dotnow/Tools/BindingGenerator/Scripts/Emit/DirectCallTypeBuilder.cs
@@ -0,0 +1,76 @@
+﻿#if !UNITY_DISABLE
+#if UNITY_EDITOR && NET_4_6
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace dotnow.BindingGenerator.Emit
+{
+    internal sealed class DirectCallTypeBuilder
+    {
+        // Private
+        private Type type = null;
+
+        // Constructor
+        public DirectCallTypeBuilder(Type type)
+        {
+            this.type = type;
+        }
+
+        // Methods
+        public CodeTypeDeclaration BuildTypeDirectCall()
+        {
+            CodeTypeDeclaration codeType = new CodeTypeDeclaration(type.Name + "_DirectCallBindings");
+            codeType.Comments.Add(new CodeCommentStatement("Generated from type: " + type.AssemblyQualifiedName));
+            codeType.Comments.Add(new CodeCommentStatement("Generated from assembly: " + type.Assembly.Location));
+            codeType.Attributes = MemberAttributes.Assembly | MemberAttributes.Static | MemberAttributes.Final;
+
+            // Add generated attribute
+            codeType.CustomAttributes.Add(new CodeAttributeDeclaration(new CodeTypeReference(typeof(GeneratedAttribute))));
+
+            HashSet<string> definedMethodNames = new HashSet<string>();
+
+            // Todo - Process all properties
+
+            // Process all methods
+            foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                // Skip object methods
+                if (method.DeclaringType == typeof(object) || method.DeclaringType == typeof(MarshalByRefObject))
+                    continue;
+
+                // Check for property - these will be handled by ProxyPropertyBuilder
+                if (method.IsSpecialName == true)
+                    continue;
+
+                // Check for already added
+                if (definedMethodNames.Contains(method.ToString()) == true)
+                    continue;
+
+                if (method.IsAbstract == false)
+                {
+                    DirectCallMethodBuilder methodBuilder = new DirectCallMethodBuilder(method);
+
+                    // Build method
+                    codeType.Members.Add(methodBuilder.BuildMethodDirectCall());
+
+                    definedMethodNames.Add(method.Name);
+                }
+            }
+
+            return codeType;
+        }
+
+        public string GetTypeFlattenedName()
+        {
+            // Check for namespace
+            if (string.IsNullOrEmpty(type.Namespace) == true)
+                return type.Name;
+
+            return string.Concat(type.Namespace.Replace('.', '_'), "_", type.Name);
+        }
+    }
+}
+#endif
+#endif
