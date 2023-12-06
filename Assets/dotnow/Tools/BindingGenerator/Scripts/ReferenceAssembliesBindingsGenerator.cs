4bf15d36a2a7f8e3d27a571857bf5a0d632e42de
DESKTOP-591D8U6\scott <scottyboy805@hotmail.co.uk>
2023-12-06 10:48:55 +0000

    Start implementing bindings generating for reference assemblies

---
+++ Assets/dotnow/Tools/BindingGenerator/Scripts/ReferenceAssembliesBindingsGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace dotnow.BindingGenerator
+{
+    [CreateAssetMenu(menuName = "dotnow/Reference Assemblies Bindings")]
+    public class ReferenceAssembliesBindingsGenerator : ScriptableObject
+    {
+        // Public
+        public string generatePath = "Assets/Bindings/Generated";
+        public string[] assemblyNameOrPaths =
+        {
+            "UnityEngine.CoreModule.dll",
+        };
+
+        // Methods
+        [ContextMenu("dotnow/Generate Bindings")]
+        public void RebuildBindingsForReferenceAssemblies()
+        {
+            // Create service
+            BindingsGeneratorService service = new BindingsGeneratorService();
+
+            // Generate bindings
+            foreach (string nameOrPath in assemblyNameOrPaths)
+            {
+                // Check for error
+                if (string.IsNullOrEmpty(nameOrPath) == true)
+                    continue;
+
+                // Get the output path
+                string outputPath = Path.GetFullPath(generatePath).Replace('\\', '/');
+
+                // Create directory
+                if (Directory.Exists(outputPath) == false)
+                    Directory.CreateDirectory(outputPath);
+
+                Debug.LogFormat("Generate Bindings: From source = {0}, Target output = {1}", nameOrPath, outputPath);
+
+                // Generate bindings
+                RebuildBindingsForReferenceAssembly(service, nameOrPath, outputPath);
+            }
+
+#if UNITY_EDITOR
+            // Update asset database
+            AssetDatabase.Refresh();
+#endif
+        }
+
+        public void RebuildBindingsForReferenceAssembly(BindingsGeneratorService service, string nameOrPath, string outputPath)
+        {
+            // Try to load
+            Assembly asm = null;
+            try
+            {
+                asm = Assembly.Load(nameOrPath);
+            }
+            catch(Exception e)
+            {
+                Debug.LogError("Bindings will not be generated for assembly: " + nameOrPath);
+                Debug.LogException(e);
+                return;
+            }
+
+            // Run generate service
+            service.GenerateBindingsForAssembly(asm, outputPath);
+        }
+    }
+}
