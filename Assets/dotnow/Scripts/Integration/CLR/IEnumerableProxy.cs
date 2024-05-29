73d8934eace991b4bfb8725316ee3e447f59c9ed
Scott <scottyboy805@hotmail.co.uk>
2024-05-29 10:16:21 +0100

    Add IEnumerable proxy

---
+++ Assets/dotnow/Scripts/Integration/CLR/IEnumerableProxy.cs
@@ -0,0 +1,28 @@
+﻿using dotnow;
+using dotnow.Interop;
+using System.Collections;
+
+namespace System
+{
+    [CLRProxyBinding(typeof(IEnumerable))]
+    public class IEnumerableProxy : ICLRProxy, IEnumerable
+    {
+        // Private
+        private CLRInstance instance;
+
+        public CLRInstance Instance
+        {
+            get { return instance; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)instance.Type.GetMethod("GetEnumerator")?.Invoke(instance, null);
+        }
+
+        public void InitializeProxy(dotnow.AppDomain domain, CLRInstance instance)
+        {
+            this.instance = instance;
+        }
+    }
+}
