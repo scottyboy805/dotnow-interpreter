d70d1e4cec67d54f27d941e53c5cf96e3af27121
Scott <scottyboy805@hotmail.co.uk>
2025-08-27 11:29:17 +0100

    Disable byref bindings for now until a correct fix can be added

--- Assets/dotnow/Scripts/Integration/Unity/DirectCallBindings.Vector2.cs
+++ Assets/dotnow/Scripts/Integration/Unity/DirectCallBindings.Vector2.cs
@@ -7,12 +7,16 @@ namespace UnityEngine
 {
     internal static partial class DirectCallBindings
     {
+        // In 6.3a5 Unity changed the math and associated classes to use `in` keyword which equates to byref in runtime.
+        // For now this is breaking bindings so the simple solution is disable it in this and newer versions until a correct fix can be implemented for bindings.
+#if !UNITY_6000_3_OR_NEWER
         [Preserve]
         [CLRMethodDirectCallBinding(typeof(Vector2), "op_Subtraction", typeof(Vector2), typeof(Vector2))]
         public static void UnityEngine_Vector2_Subtraction(StackData[] stack, int offset)
         {
             stack[offset].refValue = (Vector2)stack[offset].refValue - (Vector2)stack[offset + 1].refValue;
         }
+#endif
     }
 }
 #endif
