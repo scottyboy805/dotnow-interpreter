0bf1201cbdc021d1f6173c38a4b3fba8076182c2
Scott <scottyboy805@hotmail.co.uk>
2025-11-08 14:36:09 +0000

    Add sample direct call binding

--- Integration/Unity/DirectCallBindings.GameObject.cs
+++ Integration/Unity/DirectCallBindings.GameObject.cs
@@ -1,20 +1,22 @@
-﻿//#if !UNITY_DISABLE
-//#if (UNITY_EDITOR || UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID || UNITY_WSA || UNITY_WEBGL || UNITY_SWITCH)
-//using dotnow;
-//using dotnow.Runtime;
+﻿#if !UNITY_DISABLE
+#if (UNITY_EDITOR || UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID || UNITY_WSA || UNITY_WEBGL || UNITY_SWITCH)
+using dotnow.Interop;
 
-//namespace UnityEngine
-//{
-//    internal static partial class DirectCallBindings
-//    {
-//        [Preserve]
-//        [CLRMethodDirectCallBinding(typeof(GameObject), "get_transform")]
-//        public static void UnityEngine_GameObject_GetTransform(StackData[] stack, int offset)
-//        {
-//            stack[offset].Ref = ((GameObject)stack[offset].Ref).transform;
-//            stack[offset].Type = StackType.Ref;
-//        }
-//    }
-//}
-//#endif
-//#endif
+namespace UnityEngine
+{
+    internal static partial class DirectCallBindings
+    {
+        [Preserve]
+        [CLRMethodBinding(typeof(GameObject), "get_transform")]
+        public static void UnityEngine_GameObject_GetTransform(StackContext context)
+        {
+            // Get the game object instance from the stack, at offset 0
+            GameObject go = context.ReadArgObject<GameObject>(0);
+
+            // Write the transform back to the stack as the return value
+            context.ReturnObject<Transform>(go.transform);
+        }
+    }
+}
+#endif
+#endif
--- Integration/Unity/DirectCallBindings.Time.cs
+++ Integration/Unity/DirectCallBindings.Time.cs
@@ -1,15 +1,14 @@
 ﻿//#if !UNITY_DISABLE
 //#if (UNITY_EDITOR || UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID || UNITY_WSA || UNITY_WEBGL || UNITY_SWITCH)
-//using dotnow;
-//using dotnow.Runtime;
+//using dotnow.Interop;
 
 //namespace UnityEngine
 //{
 //    internal static partial class DirectCallBindings
 //    {
 //        [Preserve]
-//        [CLRMethodDirectCallBinding(typeof(Time), "get_time")]
-//        public static void UnityEngine_Time_GetTime(StackData[] stack, int offset)
+//        [CLRMethodBinding(typeof(Time), "get_time")]
+//        public static void UnityEngine_Time_GetTime(StackContext)
 //        {
 //            stack[offset].Single = Time.time;
 //            stack[offset].Type = StackType.Single;
