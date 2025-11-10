9246714ccfd56740051eebebc9af81059d527c2b
Scott <scottyboy805@hotmail.co.uk>
2025-11-10 15:43:06 +0000

    Add initial async/await test

---
+++ Tests/Editor/Common/Async.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+using TestAssembly;
+
+namespace dotnow.Common
+{
+    [TestFixture]
+    [Category("Unit Test")]
+    public class Async
+    {
+        [Test]
+        public void TestAsyncDelay()
+        {
+            // Try to load method
+            MethodInfo method = TestUtils.LoadTestMethod(nameof(TestAsync), nameof(TestAsync.TestDelay));
+
+            // Call original - Fix: TestAddition doesn't exist, using TestAdditionInt instead
+            object expected = TestArithmetic.TestAdditionInt();
+            object actual = method.Invoke(null, null);
+
+            // Check for equal elements
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
